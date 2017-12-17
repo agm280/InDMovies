29c47c11da4673a9ec8c5f1e76807ee73863230a
Alejandro <alex.elguti@gmail.com>
2017-12-17 14:11:24 +0100

    agm280-VistaVideo

---
+++ DSMGitGen/WebApplication1/Controllers/TemaAssembler.cs
@@ -0,0 +1,34 @@
+﻿using DSMGitGenNHibernate.EN.DSMGit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TemaAssembler
+    {
+        public static TemaModel ConvertENToModelUI(TemaEN tema)
+        {
+            TemaModel te = new TemaModel();
+            te.Id = tema.Id;
+            te.Descripcion = tema.Descripcion;
+            te.Usuario = tema.Usuario.Nick;
+            te.Estado = tema.Estado.ToString();
+            te.Titulo = tema.Titulo;
+            te.Etiquetas = tema.Etiquetas;
+            return te;
+        }
+
+        public static IList<TemaModel> ConvertListENToModel(IList<TemaEN> temas)
+        {
+            IList<TemaModel> arts = new List<TemaModel>();
+            foreach (TemaEN en in temas)
+            {
+                arts.Add(ConvertENToModelUI(en));
+            }
+            return arts;
+        }
+    }
+}
---
+++ DSMGitGen/WebApplication1/Models/TemaModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class TemaModel
+    {
+
+       
+        public virtual int Id {
+            get;
+            set;
+        }
+
+
+
+        public virtual string Descripcion
+        {
+            get;
+            set;
+        }
+
+
+
+
+        public virtual string Estado
+        {
+            get;
+            set;
+        }
+
+
+
+        public virtual string Usuario
+        {
+            get;
+            set;
+        }
+
+
+
+        public virtual string Titulo
+        {
+            get;
+            set;
+        }
+
+
+        public virtual System.Collections.Generic.IList<string> Etiquetas
+        {
+            get;
+            set;
+        }
+
+    }
+}
