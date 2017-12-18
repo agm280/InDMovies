b95b7ea9c8c98706430e8ffa0dbbccd254de9fa9
Alejandro <alex.elguti@gmail.com>
2017-12-18 16:05:54 +0100

    agm-280-Video

---
+++ DSMGitGen/WebApplication1/Controllers/VideoController.cs
@@ -0,0 +1,97 @@
+﻿using DSMGitGenNHibernate.CAD.DSMGit;
+using DSMGitGenNHibernate.EN.DSMGit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class VideoController : BasicController
+    {
+        // GET: Video
+        public ActionResult Index()
+        {
+            SessionInitialize();
+            VideoCAD videoCAD = new VideoCAD(session);
+            IList<VideoEN> videos = videoCAD.ReadAllDefault(0, -1);
+            IList<VideoModel> videoModels = VideoAssembler.convertListENToModel(videos);
+            SessionClose();
+            return View(videoModels);
+        }
+
+        // GET: Video/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Video/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Video/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Video/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Video/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Video/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Video/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DSMGitGen/WebApplication1/Models/VideoModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class VideoModel
+    {
+
+        public virtual string Titulo
+        {
+            get; set;
+        }
+
+
+
+        public virtual int Id
+        {
+            get; set;
+        }
+
+
+
+        public virtual string Descripcion
+        {
+            get; set;
+        }
+
+
+
+        public virtual System.Collections.Generic.IList<string> Etiquetas
+        {
+            get; set;
+        }
+
+
+
+        public virtual string Usuario
+        {
+            get; set;
+        }
+
+
+        public virtual string Fecha_subida
+        {
+            get; set;
+        }
+
+    }
+}
