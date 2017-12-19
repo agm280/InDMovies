7530ac898f37b3a259dd02c11f9bd4437a4458e3
Alejandro <alex.elguti@gmail.com>
2017-12-19 19:30:56 +0100

    agm280@grupoyusu

---
+++ DSMGitGen/WebApplication1/Controllers/UsuarioAssembler.cs
@@ -0,0 +1,37 @@
+﻿using DSMGitGenNHibernate.EN.DSMGit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using System.Globalization;
+
+namespace WebApplication1.Controllers
+{
+    public class UsuarioAssembler
+    {
+
+        public static UsuarioModel crearUsu(UsuarioEN usuEN) {
+
+            UsuarioModel usu = new UsuarioModel();
+
+            usu.Nick = usuEN.Nick;
+            usu.Email = usuEN.Email;
+
+            return usu;
+        }
+
+        public static IList<UsuarioModel> crearListaUsus(IList<UsuarioEN> usuEN)
+        {
+
+            IList<UsuarioModel> usu = new List<UsuarioModel>();
+
+            foreach (UsuarioEN u in usuEN) {
+
+                usu.Add(crearUsu(u));
+            }
+
+            return usu;
+        }
+    }
+}
---
+++ DSMGitGen/WebApplication1/Controllers/UsuarioController.cs
@@ -0,0 +1,101 @@
+﻿using DSMGitGenNHibernate.CAD.DSMGit;
+using DSMGitGenNHibernate.EN.DSMGit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UsuarioController : BasicController
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        // GET: Usuario/Details/5
+        public ActionResult DetailsGrupo(string id)
+        {
+            SessionInitialize();
+            UsuarioCAD usuarioCAD = new UsuarioCAD(session);
+            IList<UsuarioEN> l_usurarioEN = usuarioCAD.DameUsuarioPorGrupo(id);
+            IEnumerable<UsuarioModel> usuarios = UsuarioAssembler.crearListaUsus(l_usurarioEN).ToList();
+            return PartialView("~/Views/Usuario/List.cshtml",usuarios);
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
