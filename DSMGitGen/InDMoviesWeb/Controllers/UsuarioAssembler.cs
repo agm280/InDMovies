b3602e492355e3cbcb53177389ccd04d4220e6b0
AMG97 <alvaromolinag97@gmail.com>
2017-12-29 13:55:24 +0100

    Funciona pls

---
+++ DSMGitGen/InDMoviesWeb/Controllers/UsuarioAssembler.cs
@@ -0,0 +1,66 @@
+﻿using DSMGitGenNHibernate.EN.DSMGit;
+using InDMoviesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InDMoviesWeb.Controllers
+{
+    public class UsuarioAssembler
+    {
+        public static UsuarioModel ConvertENToModel(UsuarioEN usuEN)
+        {
+            UsuarioModel usu = new UsuarioModel();
+
+            usu.Nick = usuEN.Nick;
+            usu.Email = usuEN.Email;
+            usu.Nombre = usuEN.Nombre;
+            usu.Apellidos = usuEN.Apellidos;
+            try
+            {
+                string parsedDate = usuEN.Fecha_nac.ToString();
+                string[] fecha = parsedDate.Split(' ');
+                usu.Fecha_nac = fecha[0];
+
+            }
+            catch
+            {
+                usu.Fecha_nac = "NO hay fecha";
+            }
+            usu.Descripcion = usuEN.Descripcion;
+            usu.Imagen = usuEN.Imagen;
+            switch (usuEN.Rol)
+            {
+                case (DSMGitGenNHibernate.Enumerated.DSMGit.RolEnum)1:
+                    usu.Rol = "Director";
+                    break;
+                case (DSMGitGenNHibernate.Enumerated.DSMGit.RolEnum)2:
+                    usu.Rol = "Camara";
+                    break;
+                case (DSMGitGenNHibernate.Enumerated.DSMGit.RolEnum)3:
+                    usu.Rol = "Guionista";
+                    break;
+                case (DSMGitGenNHibernate.Enumerated.DSMGit.RolEnum)4:
+                    usu.Rol = "Actor";
+                    break;
+                case (DSMGitGenNHibernate.Enumerated.DSMGit.RolEnum)5:
+                    usu.Rol = "Editor";
+                    break;
+                default:
+                    usu.Rol = "Ninguno";
+                    break;
+            }
+            return usu;
+        }
+        public static IList<UsuarioModel> ConvertListENToModel(IList<UsuarioEN> usuEN)
+        {
+            IList<UsuarioModel> usu = new List<UsuarioModel>();
+            foreach (UsuarioEN u in usuEN)
+            {
+                usu.Add(ConvertENToModel(u));
+            }
+            return usu;
+        }
+    }
+}
---
+++ DSMGitGen/InDMoviesWeb/Models/UsuarioModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InDMoviesWeb.Models
+{
+    public class UsuarioModel
+    {
+        [Key]
+        public virtual string Email
+        {
+            get; set;
+        }
+        public virtual string Nombre
+        {
+            get; set;
+        }
+        public virtual string Apellidos
+        {
+            get; set;
+        }
+        public virtual string Nick
+        {
+            get; set;
+        }
+
+        public virtual string Fecha_nac
+        {
+            get; set;
+        }
+        public virtual string Rol
+        {
+            get; set;
+        }
+        public virtual string Imagen
+        {
+            get; set;
+        }
+        public virtual string Descripcion
+        {
+            get; set;
+        }
+    }
+}
