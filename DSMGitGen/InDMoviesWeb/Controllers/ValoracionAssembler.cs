8090fc4b078b3dd99be672729835aff792d7bbc9
Enrique <quiquesoyyo@gmail.com>
2018-01-08 11:23:20 +0100

    evc23 VideoVideoVideo

---
+++ DSMGitGen/InDMoviesWeb/Controllers/ValoracionAssembler.cs
@@ -0,0 +1,44 @@
+﻿using DSMGitGenNHibernate.EN.DSMGit;
+using InDMoviesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InDMoviesWeb.Controllers
+{
+    public class ValoracionAssembler
+    {
+        public static ValoracionModel convertENToModelUI(ValoracionEN valoracionEN)
+        {
+
+            ValoracionModel v = new ValoracionModel();
+
+            v.Id = valoracionEN.Id;
+            v.Valor = valoracionEN.Valor;
+            v.Usuario = valoracionEN.Usuario.Nick;
+            v.Video = valoracionEN.Video.Id;
+
+            return v;
+
+        }
+
+        public static IList<ValoracionModel> convertListENToModel(IList<ValoracionEN> valoraciones)
+        {
+
+            IList<ValoracionModel> valoracionModels = new List<ValoracionModel>();
+
+            foreach (ValoracionEN v in valoraciones)
+            {
+
+                ValoracionModel valoracionM = convertENToModelUI(v);
+
+                valoracionModels.Add(valoracionM);
+            }
+
+            return valoracionModels;
+        }
+
+
+    }
+}
---
+++ DSMGitGen/InDMoviesWeb/Controllers/ValoracionController.cs
@@ -0,0 +1,104 @@
+﻿using DSMGitGenNHibernate.CAD.DSMGit;
+using DSMGitGenNHibernate.CEN.DSMGit;
+using DSMGitGenNHibernate.EN.DSMGit;
+using InDMoviesWeb.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InDMoviesWeb.Controllers
+{
+    public class ValoracionController : BasicController
+    {
+        // GET: Valoracion
+        public ActionResult Index()
+        {
+            SessionInitialize();
+            ValoracionCAD valoracionCAD = new ValoracionCAD(session);
+            IList<ValoracionEN> valoraciones = valoracionCAD.ReadAllDefault(0, -1);
+            IList<ValoracionModel> valoracionModels = ValoracionAssembler.convertListENToModel(valoraciones);
+            SessionClose();
+            return View(valoracionModels);
+        }
+
+        // GET: Valoracion/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Valoracion/Create
+        public ActionResult Create(int id)
+        {
+            return PartialView();
+        }
+
+        // POST: Valoracion/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, int id)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ValoracionCEN valoracionCEN = new ValoracionCEN();
+                string auxS = collection["Valor"];
+                int auxI;
+                int.TryParse(auxS, out auxI);
+                valoracionCEN.New_(p_valor: auxI, p_usuario: User.Identity.GetUserName(), p_video: id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Valoracion/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Valoracion/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Valoracion/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Valoracion/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
